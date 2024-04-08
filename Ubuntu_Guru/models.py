a12113cfcd0f6f2c7a6b3bb73d07f71ddd82ea3d
PACIFIC645 <stqpictures@gmail.com>
2024-04-08 17:16:23 +0200

    Add docstrings to the Article model in models.py

--- Ubuntu_Guru/models.py
+++ Ubuntu_Guru/models.py
@@ -2,13 +2,22 @@
 
 from django.db import models
 
-# Create your models here.
-from django.db import models
-
 class Article(models.Model):
+    """
+    Represents a written article.
+
+    Attributes:
+        title (str): The title of the article.
+        content (str): The body content of the article.
+        published_date (datetime): The date and time the article was published.
+    """
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        """
+        Returns a string representation of the article, which is its title.
+        """
         return self.title
+        
