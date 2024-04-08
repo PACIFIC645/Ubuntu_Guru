587a8b8ace04969b67039f03151b6ec21eb60b9f
PACIFIC645 <stqpictures@gmail.com>
2024-04-08 17:22:33 +0200

    Add docstrings to the Polls urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -5,19 +5,15 @@ from . import views
 from .views import poll_tracker_season
 from .views import view_polls
 
-app_name = 'polls'  # Define the app namespace
+app_name = 'polls'   # Namespace for app-specific URLs
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Link to index view as the main page for polls
+    path('', views.index, name='index'),  # Main page for polls
     path('poll-tracker-season/', views.poll_tracker_season, name='poll_tracker_season'),
-    path('<int:question_id>/', views.detail, name='detail'),  # Link to detail view for a specific question
-    path('<int:question_id>/results/', views.results, name='results'),  # Link to results view for a specific question
-    path('<int:question_id>/vote/', views.vote, name='vote'),  # Link to vote action for a specific question
+    path('<int:question_id>/', views.detail, name='detail'),  # Detailed view for a specific question
+    path('<int:question_id>/results/', views.results, name='results'),  # Results view for specific questions
+    path('<int:question_id>/vote/', views.vote, name='vote'),  # Voting action for a specific question
     path('manage/', views.manage_polls_view, name='manage_polls'), # Manage Polls view
     path('view-polls/', view_polls, name='view_polls'),
     path('view-polls-live/', views.view_polls_live, name='view_polls_live'),
 ]
-
-
-
-
