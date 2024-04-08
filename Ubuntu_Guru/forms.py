769801647292cdaa7d545008f4e580ad9749a079
PACIFIC645 <stqpictures@gmail.com>
2024-04-08 17:25:26 +0200

    Add docstrings to forms.py

--- Ubuntu_Guru/forms.py
+++ Ubuntu_Guru/forms.py
@@ -1,12 +1,14 @@
-# form.py
+# forms.py
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    # Additional fields can be added here
-
+    """
+    Extends the default UserCreationForm to include username, email, and password fields explicitly.
+    Used for registering new users from the frontend.
+    """
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2',)
